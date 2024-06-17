de64a80f23682cae9f108a1b2e93df30533bbeba
CarlosRofts <62457373+CarlosRofts@users.noreply.github.com>
2024-06-16 18:30:31 -0600

    chore: folder estructure

--- src/js/page1.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-unresolved
-import css from '../css/inside.css?raw';
-
-// console.log('css', css);
-export const styles = css;
-export function init() {
-  console.log('⭐✨⭐✨ page 1 ⭐✨⭐✨');
-}
