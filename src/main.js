979ca42ece1676aa15ca776c8f5087ff8030c63b
CarlosRofts <62457373+CarlosRofts@users.noreply.github.com>
2024-06-18 23:04:06 -0600

    feat: redirection path & animationSVG

--- src/main.js
+++ src/main.js
@@ -1,21 +1,34 @@
-import './style.css';
-// import setupCounter from './counter';
+import './css/colors.css';
+import './css/base.css';
+import './css/layout.css';
+import './css/theme.css';
+import './css/libs/bootstrap-grid.css';
+import './css/fonts.css';
 import Sketch from './threeScene';
+import animateScrollSvg from './js/animations';
 
-import fetchProjects from './fetchProjects';
+// import fetchProjects from './fetchProjects';
 
-async function obtenerProyectos() {
-  try {
-    const proyectos = await fetchProjects();
-    console.log(proyectos);
-  } catch (error) {
-    console.error('Error al obtener proyectos:', error);
-  }
-}
+// async function obtenerProyectos() {
+//   try {
+//     const proyectos = await fetchProjects();
+//     console.log(proyectos);
+//   } catch (error) {
+//     console.error('Error al obtener proyectos:', error);
+//   }
+// }
 
 // Llamada a la función para obtener proyectos
 // obtenerProyectos();
 
+// 🔨 hacer alguna animacion de entrada o loader
+
+if (window.location.pathname !== '/') {
+  if (window.location.pathname !== '/index.html') window.location.href = '/';
+}
+
 new Sketch({
   domElement: document.getElementById('container'),
 });
+
+animateScrollSvg();
