71655d85cb35e9e5d464680b8d5ec4a725472052
CarlosRofts <62457373+CarlosRofts@users.noreply.github.com>
2024-06-19 11:21:19 -0600

    deploy test

--- src/js/pages/page1.js
+++ src/js/pages/page1.js
@@ -4,58 +4,58 @@ import css from '../../css/pages/page1.css?raw';
 import getCurrentData from '../getCurrentData';
 
 function renderProject(project) {
-	console.log('mport.meta.env.BASE_URL', import.meta.env.BASE_URL);
-	debugger;
+  console.log('mport.meta.env.BASE_URL', import.meta.env.BASE_URL);
+  const baseurl = import.meta.env.BASE_URL;
 
-	// Renderizar la imagen superior
-	const topImgElement = document.getElementById('topImg');
-	if (topImgElement) {
-		topImgElement.style.backgroundImage = `url(img/${project.topImg})`;
-	}
+  // Renderizar la imagen superior
+  const topImgElement = document.getElementById('topImg');
+  if (topImgElement) {
+    topImgElement.style.backgroundImage = `url(${baseurl}/img/${project.topImg})`;
+  }
 
-	const topInfoContainer = document.getElementById('topInfoContainer');
-	const tecs = topInfoContainer.querySelector('#tecs');
-	project.tecs.forEach((tec) => {
-		tecs.innerHTML += `<img class="me-2" src="/img/tecs/${tec}" alt="" />`;
-	});
-	const infotxt1 = topInfoContainer.querySelector('#infotxt1');
-	infotxt1.innerHTML = project.infotxt;
-	const livedemo = topInfoContainer.querySelector('#livedemo');
-	livedemo.innerHTML = `<a href="${project.livedemo}" >${project.livedemo}</a>`;
+  const topInfoContainer = document.getElementById('topInfoContainer');
+  const tecs = topInfoContainer.querySelector('#tecs');
+  project.tecs.forEach((tec) => {
+    tecs.innerHTML += `<img class="me-2" src="${baseurl}/img/tecs/${tec}" alt="" />`;
+  });
+  const infotxt1 = topInfoContainer.querySelector('#infotxt1');
+  infotxt1.innerHTML = project.infotxt;
+  const livedemo = topInfoContainer.querySelector('#livedemo');
+  livedemo.innerHTML = `<a href="${project.livedemo}" >${project.livedemo}</a>`;
 
-	// Renderizar el título
-	const titleElement = document.getElementById('title');
-	if (titleElement) {
-		titleElement.innerHTML = project.title;
-	}
+  // Renderizar el título
+  const titleElement = document.getElementById('title');
+  if (titleElement) {
+    titleElement.innerHTML = project.title;
+  }
 
-	// Renderizar las imágenes y textos
-	project.images.forEach((image, index) => {
-		const imgElement = document.getElementById(`img${index + 1}`);
-		const textElement = document.getElementById(`text${index + 1}`);
+  // Renderizar las imágenes y textos
+  project.images.forEach((image, index) => {
+    const imgElement = document.getElementById(`img${index + 1}`);
+    const textElement = document.getElementById(`text${index + 1}`);
 
-		if (imgElement) {
-			imgElement.src = `/img/${image}`;
-		}
+    if (imgElement) {
+      imgElement.src = `${baseurl}/img/${image}`;
+    }
 
-		if (textElement) {
-			textElement.innerHTML = project.texts[index] || '';
-		}
-	});
+    if (textElement) {
+      textElement.innerHTML = project.texts[index] || '';
+    }
+  });
 
-	document.querySelector('.inside').style.opacity = 1;
+  document.querySelector('.inside').style.opacity = 1;
 }
 
 export const styles = css;
 
 export function init(currentLinkId, asscroll) {
-	// console.log('⭐✨⭐✨ page 1 ⭐✨⭐✨');
-	console.log('init id-project:', currentLinkId);
-	const dataProject = getCurrentData(currentLinkId);
-	renderProject(dataProject);
-	imagesLoaded(document.querySelector('body'), (instance) => {
-		console.log('all images are loaded');
-		asscroll.update();
-		asscroll.resize();
-	});
+  // console.log('⭐✨⭐✨ page 1 ⭐✨⭐✨');
+  console.log('init id-project:', currentLinkId);
+  const dataProject = getCurrentData(currentLinkId);
+  renderProject(dataProject);
+  imagesLoaded(document.querySelector('body'), (instance) => {
+    console.log('all images are loaded');
+    asscroll.update();
+    asscroll.resize();
+  });
 }
